25fbf65668cb93b0b7f3583a3ef34dd4777edf41
B0T-Josh <jbsantos18.jbs@gmail.com>
2025-09-06 23:44:45 +0800

    Functionalities

--- attendance/app/authPages/register/page.tsx
+++ attendance/app/authPages/register/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 import { useState, useEffect } from "react";
-import { addUser, encryptPassword } from "../api/endpoints";
+import { addUser, encryptPassword } from "../../api/endpoints";
 import Link from "next/link";
 
 export default function RegisterPage() {
   const [loaded, setLoaded] = useState(false);
   const [loading, setLoading] = useState(false);
   const [info, setInfo ] = useState({
-      email: "",
-      password: "",
-      name: "",
-      subjects: "",
+      email: null,
+      password: null,
+      name: null,
+      subjects: null,
   });
 
     const handleSubmit = async () => {
@@ -61,28 +61,12 @@ export default function RegisterPage() {
         </h1>
         <input className={`shadow-xl bg-zinc-800 transition-opacity ease-out duration-1000 w-full px-4 py-2 rounded-lg focus:outline-none ${loaded ? "animate-fadeInUp delay-[400ms]" : "opacity-0"}`}type="text" name="subjects" onChange={handleChange}/>
 
-<<<<<<< HEAD:attendance/app/register/page.tsx
-        {/* <Link href="/authenticationPages/loginPage"> */}
-=======
-        <Link href="/authPages/login">
->>>>>>> origin/main:attendance/app/authPages/register/page.tsx
-          <div className="mt-6 transition-all ease-in-out hover:-translate-y-1 hover:scale-105 duration-300 w-full">
-            <button className={`mt-4 cursor-pointer shadow-xl bg-purple-800 transition-all ease-out duration-1000 w-full px-4 py-2 rounded-lg ${loaded ? "animate-fadeInUp delay-[600ms]" : "opacity-0"}`} type="submit" onClick={handleSubmit} disabled={loading}>
-              Register
-            </button>
-          </div>
-<<<<<<< HEAD:attendance/app/register/page.tsx
-        {/* </Link> */}
-=======
-        </Link>
-
-        <h3 className={`mt-4 transition-opacity ease-out duration-1000 text-center text-gray-600 ${loaded ? "animate-fadeInUp delay-[500ms]" : "opacity-0"}`}>
-          Already have an account? 
-          <Link href="/authPages/login" className="ml-1 text-purple-800 hover:text-purple-600">
-            Sign In
-          </Link>
-        </h3>
->>>>>>> origin/main:attendance/app/authPages/register/page.tsx
+        
+        <div className="mt-6 transition-all ease-in-out hover:-translate-y-1 hover:scale-105 duration-300 w-full">
+          <button className={`mt-4 cursor-pointer shadow-xl bg-purple-800 transition-all ease-out duration-1000 w-full px-4 py-2 rounded-lg ${loaded ? "animate-fadeInUp delay-[600ms]" : "opacity-0"}`} type="submit" onClick={handleSubmit} disabled={loading}>
+            Register
+          </button>
+        </div>
       </div>
     </div>
   );
