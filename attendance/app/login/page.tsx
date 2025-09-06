741792638a4f350b11fe97210675ce7f0657cb0b
Noctou <noctouu@gmail.com>
2025-09-06 21:30:28 +0800

    added custom auth pages

---
+++ attendance/app/login/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+export default function logIn() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="space-y-4 w-80">
+        <h1 className={`transition-opacity ease-out duration-1000 ${loaded ? "animate-fadeInUp delay-[100ms]" : "opacity-0"}`}>
+          Email:
+        </h1>
+        <input className={`shadow-xl bg-zinc-800 transition-opacity ease-out duration-1000 w-full px-4 py-2 rounded-lg focus:outline-none ${loaded ? "animate-fadeInUp delay-[200ms]" : "opacity-0"}`} type="text"/>
+
+        <h1 className={`transition-opacity ease-out duration-1000 ${loaded ? "animate-fadeInUp delay-[300ms]" : "opacity-0"}`}>
+          Password:
+        </h1>
+        <input className={`shadow-xl bg-zinc-800 transition-opacity ease-out duration-1000 w-full px-4 py-2 rounded-lg focus:outline-none ${loaded ? "animate-fadeInUp delay-[400ms]" : "opacity-0"}`}type="password"/>
+
+        <div className="flex justify-end">
+          <Link href="/auth/forgot-password" className={`mb-2 transition-opacity ease-out duration-1000 text-purple-800 hover:text-purple-600 ${loaded ? "animate-fadeInUp delay-[500ms]" : "opacity-0"}`}>
+            Forgot Password?
+          </Link>
+        </div>
+
+        <Link href="/dashboard">
+          <div className="transition-all ease-in-out hover:-translate-y-1 hover:scale-105 duration-300 w-full">
+            <button className={`cursor-pointer shadow-xl bg-purple-800 hover:bg-purple-600 transition-all ease-out duration-1000 w-full px-4 py-2 rounded-lg ${loaded ? "animate-fadeInUp delay-[600ms]" : "opacity-0"}`}>
+              Log In
+            </button>
+          </div>
+        </Link>
+
+        <h3 className={`mt-4 transition-opacity ease-out duration-1000 text-center text-gray-600 ${loaded ? "animate-fadeInUp delay-[500ms]" : "opacity-0"}`}>
+          Don't have an account? 
+          <Link href="/register" className="ml-1 text-[#8854cc] hover:text-purple-600">
+            Sign Up
+          </Link>
+        </h3>
+      </div>
+    </div>
+  );
+}
--- attendance/app/register/page.tsx
+++ attendance/app/register/page.tsx
@@ -1,82 +1,46 @@
 "use client";
-import { useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-import { addUser } from "../api/endpoints";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
-);
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
-export default function RegisterPage() {
-    const [loading, setLoading] = useState(false);
-    const [info, setInfo ] = useState({
-        email: "",
-        password: "",
-        name: "",
-        subjects: "",
-    });
+export default function registerPage() {
+  const [loaded, setLoaded] = useState(false);
 
-    const handleSubmit = async () => {
-        setLoading(true);
-        if(await addUser(supabase, info)) {
-            alert("Profile created");
-            setLoading(false);  
-        } else {alert("Error creating profile"); setLoading(false);}
-    }
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
 
-    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-        setInfo({
-            ...info,
-            [e.target.name]: e.target.value
-        });
-    }
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="space-y-4 w-80">
+        <h1 className={`transition-opacity ease-out duration-1000 ${loaded ? "animate-fadeInUp delay-[100ms]" : "opacity-0"}`}>
+          Email:
+        </h1>
+        <input className={`shadow-xl bg-zinc-800 transition-opacity ease-out duration-1000 w-full px-4 py-2 rounded-lg focus:outline-none ${loaded ? "animate-fadeInUp delay-[200ms]" : "opacity-0"}`} type="text"/>
 
-    return(
-        <div className="p-4">
-        <input
-            type="text"
-            name="email"
-            placeholder="email"
-            value={info.email}
-            onChange={handleChange}
-            className="border p-2 mr-2"
-        />
-        <input
-            type="password"
-            name="password"
-            placeholder="password"
-            value={info.password}
-            onChange={handleChange}
-            className="border p-2 mr-2"
-        />
-        <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            value={info.name}
-            onChange={handleChange}
-            className="border p-2 mr-2"
-        />
-        <input
-            type="text"
-            name="subjects"
-            placeholder="Subject(s)"
-            value={info.subjects}
-            onChange={handleChange}
-            className="border p-2 mr-2"
-        />
-        <button
-            onClick={handleSubmit}
-            className="bg-blue-500 text-white px-4 py-2"
-        >
-            Add Profile
-        </button>
-        <br />
-        <br />
-        <br />
-        <br />
-        <p>{loading ? "Loading" : ""}</p>
+        <h1 className={`transition-opacity ease-out duration-1000 ${loaded ? "animate-fadeInUp delay-[300ms]" : "opacity-0"}`}>
+          Password:
+        </h1>
+        <input className={`shadow-xl bg-zinc-800 transition-opacity ease-out duration-1000 w-full px-4 py-2 rounded-lg focus:outline-none ${loaded ? "animate-fadeInUp delay-[400ms]" : "opacity-0"}`}type="password"/>
+
+        <h1 className={`transition-opacity ease-out duration-1000 ${loaded ? "animate-fadeInUp delay-[300ms]" : "opacity-0"}`}>
+          Name:
+        </h1>
+        <input className={`shadow-xl bg-zinc-800 transition-opacity ease-out duration-1000 w-full px-4 py-2 rounded-lg focus:outline-none ${loaded ? "animate-fadeInUp delay-[400ms]" : "opacity-0"}`}type="text"/>
+
+        <h1 className={`transition-opacity ease-out duration-1000 ${loaded ? "animate-fadeInUp delay-[300ms]" : "opacity-0"}`}>
+          Course:
+        </h1>
+        <input className={`shadow-xl bg-zinc-800 transition-opacity ease-out duration-1000 w-full px-4 py-2 rounded-lg focus:outline-none ${loaded ? "animate-fadeInUp delay-[400ms]" : "opacity-0"}`}type="text"/>
+
+        <Link href="/authenticationPages/loginPage">
+          <div className="mt-6 transition-all ease-in-out hover:-translate-y-1 hover:scale-105 duration-300 w-full">
+            <button className={`mt-4 cursor-pointer shadow-xl bg-purple-800 transition-all ease-out duration-1000 w-full px-4 py-2 rounded-lg ${loaded ? "animate-fadeInUp delay-[600ms]" : "opacity-0"}`}>
+              Register
+            </button>
+          </div>
+        </Link>
+      </div>
     </div>
-    );
+  );
 }
