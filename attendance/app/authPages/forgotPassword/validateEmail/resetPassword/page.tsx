f5e9b22002cc1aace3a843fd2c8e5fb3be47997c
Noctou <noctouu@gmail.com>
2025-09-07 21:38:13 +0800

    added additional files

--- attendance/app/authPages/forgotPassword/validateEmail/resetPassword/page.tsx
+++ attendance/app/authPages/forgotPassword/validateEmail/resetPassword/page.tsx
@@ -14,12 +14,17 @@ export default function ForgotPassword() {
     <div className="flex items-center justify-center min-h-screen">
         <div className="space-y-4 w-80">
             <h1 className={`mb-4 transition-opacity ease-out duration-1000 ${loaded ? "animate-fadeInUp delay-[100ms]" : "opacity-0"}`}>
-                Email:
+                Set a new password:
             </h1>
-            <input className={`shadow-xl bg-zinc-800 transition-opacity ease-out duration-1000 w-full px-4 py-2 rounded-lg focus:outline-none ${loaded ? "animate-fadeInUp delay-[200ms]" : "opacity-0"}`} type="text" placeholder="example@gmail.com"/>
+            <input className={`shadow-xl bg-zinc-800 transition-opacity ease-out duration-1000 w-full px-4 py-2 rounded-lg focus:outline-none ${loaded ? "animate-fadeInUp delay-[200ms]" : "opacity-0"}`} type="password"/>
+
+            <h1 className={`mb-4 transition-opacity ease-out duration-1000 ${loaded ? "animate-fadeInUp delay-[100ms]" : "opacity-0"}`}>
+                Confirm password:
+            </h1>
+            <input className={`shadow-xl bg-zinc-800 transition-opacity ease-out duration-1000 w-full px-4 py-2 rounded-lg focus:outline-none ${loaded ? "animate-fadeInUp delay-[200ms]" : "opacity-0"}`} type="password"/>
 
             <div className="transition-all ease-in-out hover:-translate-y-1 hover:scale-105 duration-300 w-full">
-                <Link href='/authPages/forgotPassword/validateEmail'>
+                <Link href='/authPages/login'>
                     <button className={`mt-4 cursor-pointer shadow-xl bg-purple-800 hover:bg-purple-600 transition-all ease-out duration-1000 w-full px-4 py-2 rounded-lg ${loaded ? "animate-fadeInUp delay-[600ms]" : "opacity-0"}`} type="submit">
                         Next
                     </button>
