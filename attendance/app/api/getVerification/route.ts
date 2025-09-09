6254b0caa9809b6e8e33742fd8c78184ad5e62d8
B0T-Josh <jbsantos18.jbs@gmail.com>
2025-09-09 14:41:30 +0800

    Verification code added

--- attendance/app/api/getVerification/route.ts
+++ attendance/app/api/getVerification/route.ts
@@ -1,13 +1,12 @@
 import { NextResponse } from "next/server";
 import { verificationFromUser } from "../endpoints"; // adjust path to your supabase client
 
-export async function GET(req: Request) {
+export async function POST(req: Request) {
   try {
-    const data: any = req.json();
-    const res: any = await verificationFromUser(parseInt(data.id)) 
-    return NextResponse.json({ message: "Value returned", verification: res.data });
+    const data: any = await req.json();
+    const res: any = await verificationFromUser(parseInt(data.id))
+    return NextResponse.json({ message: "Value returned", verification: res.verification }, {status: 200});
   } catch (err) {
-    alert(err);
-    return NextResponse.json({ error: "Server error" }, { status: 500 });
+    return NextResponse.json({ error: `${err}` }, {status: 500});
   }
 }
