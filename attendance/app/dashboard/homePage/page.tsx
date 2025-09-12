e96acf5843ddfdc6701f57e6913d1b6e91e058f7
Noctou <noctouu@gmail.com>
2025-09-12 22:20:34 +0800

    responsive containers

--- attendance/app/dashboard/homePage/page.tsx
+++ attendance/app/dashboard/homePage/page.tsx
@@ -4,19 +4,22 @@ export default function HomePage() {
     return (
         <div className="flex min-h-screen">
             <Sidebar />
-            <div className="flex-1 p-6 grid grid-cols-1 md:grid-cols-3 gap-6">
-                <div className="col-span-2 flex flex-col gap-6">
-                    <div className="bg-[#2e2e2ec0] rounded-lg h-full">
-                        
-                    </div>
 
-                    <div className="bg-[#2e2e2ec0] rounded-lg h-full">
-                        
-                    </div>
-                </div>
-
-                <div className="bg-[#2e2e2ec0] rounded-lg h-full">
+            <div className="flex-1 p-6">
+                <div className="flex flex-col md:flex-row gap-6 h-full">
                     
+                    <div className="flex-1 flex flex-col gap-6">
+                        <div className="bg-[#2e2e2ec0] rounded-lg flex-1 transition-all duration-500 hover:flex-[3]">
+                            
+                        </div>
+                        <div className="bg-[#2e2e2ec0] rounded-lg flex-1 transition-all duration-500 hover:flex-[3]">
+                            
+                        </div>
+                    </div>
+
+                    <div className="flex-1 bg-[#2e2e2ec0] rounded-lg transition-all duration-500 hover:flex-[3] h-full">
+                        
+                    </div>
                 </div>
             </div>
         </div>
