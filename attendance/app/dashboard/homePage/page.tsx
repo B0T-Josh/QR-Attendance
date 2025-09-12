020a2eaf6d033a584f4c81727d881fe00a852419
Noctou <noctouu@gmail.com>
2025-09-12 22:34:37 +0800

    named containers

--- attendance/app/dashboard/homePage/page.tsx
+++ attendance/app/dashboard/homePage/page.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "@/components/Sidebar";
+import Link from "next/link";
 
 export default function HomePage() {
     return (
@@ -7,18 +8,30 @@ export default function HomePage() {
 
             <div className="flex-1 p-6">
                 <div className="flex flex-col md:flex-row gap-6 h-full">
-                    
                     <div className="flex-1 flex flex-col gap-6">
                         <div className="bg-[#2e2e2ec0] rounded-lg flex-1 transition-all duration-500 hover:flex-[3]">
-                            
+                            <Link href={"/dashboard/addRemoveStudents"}>
+                                <div className="p-4 h-full flex justify-left">
+                                    Students Masterlist:
+                                </div>
+                            </Link>
                         </div>
+                        
                         <div className="bg-[#2e2e2ec0] rounded-lg flex-1 transition-all duration-500 hover:flex-[3]">
-                            
+                            <Link href={"/dashboard/addRemoveSubject"}>
+                                <div className="p-4 h-full flex justify-left">
+                                    Subjects:
+                                </div>
+                            </Link>
                         </div>
                     </div>
 
                     <div className="flex-1 bg-[#2e2e2ec0] rounded-lg transition-all duration-500 hover:flex-[3] h-full">
-                        
+                        <Link href={"/dashboard/StudentRecords"}>
+                            <div className="p-4 h-full flex justify-left">
+                                Student Attendance Records:
+                            </div>
+                        </Link>
                     </div>
                 </div>
             </div>
