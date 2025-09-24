6eee9b72e7612f4c91904744eedb9a21433dd48d
B0T-Josh <jbsantos18.jbs@gmail.com>
2025-09-24 22:45:57 +0800

    vercel fix

--- attendance/tools/validateID.ts
+++
@@ -1,9 +0,0 @@
-import { getId } from "@/tools/getId";
-import { useRouter } from 'next/navigation';
-
-export function validateId() {
-    const route = useRouter();
-    if(parseInt(getId() || '0') <= 0) {
-        route.push("/authPages/login");
-    }
-}
